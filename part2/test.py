4562fd862a502bcd0d6cd27ca0429dbb3c72b69f
Anders Vrethem <anders.vrethem@hiq.se>
2020-01-23 12:10:21 +0100

    Cleanup strucutre

--- part2/test.py
+++ part2/test.py
@@ -41,6 +41,36 @@ class WasRun(TestCase):
     self.log += "tearDown "
 
 
+class TestResult:
+  def __init__(self):
+    self.runCount = 0
+    self.errorCount = 0
+
+  def testStarted(self):
+    self.runCount += 1
+
+  def testFailed(self):
+    self.errorCount += 1
+
+  def summary(self):
+    return f"{self.runCount} run, {self.errorCount} failed"
+
+
+class TestSuite:
+  def __init__(self):
+    self.tests = []
+
+  def add(self, test):
+    self.tests.append(test)
+
+    def run(self):
+      result = TestResult()
+      for test in self.tests:
+        test.run(result)
+      print("result", result)
+      return result
+
+
 class TestCaseTest(TestCase):
   def setUp(self):
     self.test = WasRun("testMethod")
@@ -72,36 +102,6 @@ class TestCaseTest(TestCase):
     suite.add(WasRun("testBrokenMethod"))
     result = suite.run()
     assert("2 run, 1 failed qweqwe" == result.summary())
-
-
-class TestResult:
-  def __init__(self):
-    self.runCount = 0
-    self.errorCount = 0
-
-  def testStarted(self):
-    self.runCount += 1
-
-  def testFailed(self):
-    self.errorCount += 1
-
-  def summary(self):
-    return f"{self.runCount} run, {self.errorCount} failed"
-
-
-class TestSuite:
-  def __init__(self):
-    self.tests = []
-
-  def add(self, test):
-    self.tests.append(test)
-
-    def run(self):
-      result = TestResult()
-      for test in self.tests:
-        test.run(result)
-      print("result", result)
-      return result
 
 
 print(TestCaseTest("testTemplateMethod").run().summary())
