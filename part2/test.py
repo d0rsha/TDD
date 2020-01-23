ab114e0ea79f028c42ac26402560c29ece9829f1
Anders Vrethem <anders.vrethem@hiq.se>
2020-01-23 11:17:39 +0100

    Change to variable

--- part2/test.py
+++ part2/test.py
@@ -52,7 +52,10 @@ class TestCaseTest(TestCase):
 
 class TestResult:
   def __init__(self):
-    self.runCount = 1
+    self.runCount = 0
+
+  def testStarted(self):
+    self.runCount = self.runCount + 1
 
   def summary(self):
     return f"{self.runCount} run, 0 failed"
