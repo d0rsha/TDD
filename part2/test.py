b62f384e731f668a8cf22212bdbace6c0289848b
Anders Vrethem <anders.vrethem@hiq.se>
2020-01-23 10:17:10 +0100

    Delete testRUnning and generalize testSetUp

--- part2/test.py
+++ part2/test.py
@@ -28,12 +28,7 @@ class TestCaseTest(TestCase):
   def setUp(self):
     self.test = WasRun("testMethod")
 
-  def testRunning(self):
-    # assert(not self.test.wasRun)
-    self.test.run()
-    assert(self.test.wasRun)
-
-  def testSetUp(self):
+  def testTemplateMethod(self):
     self.test.run()
     assert("setUp testMethod " == self.test.log)
 
